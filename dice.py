723015f72920db1cc539fcb67bb2539980521416
Kiran Babu <kiranbabu3355@gmail.com>
2023-11-05 09:55:31 +0530

    Finished

---
+++ dice.py
@@ -0,0 +1,51 @@
+import random
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+dice=[]
+
+total=0
+
+no_of_dice=int(input("How Many Dice? :"))
+
+for die in range(no_of_dice):
+  dice.append(random.randint(1,6))
+
+for die in range(no_of_dice):
+    for line in dice_art.get(dice[die]):
+        print(line)
+
+for die in dice:
+    total +=die
+print(f"Total:{total}")
